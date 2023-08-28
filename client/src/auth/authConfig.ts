a3c6fcea474b67cc619887f13055c493ca7727cc
louisektw <lwegrelius@gmail.com>
2023-08-28 21:58:38 +0200

    Clean auth folder

--- client/src/auth/authConfig.ts
+++ client/src/auth/authConfig.ts
@@ -1,6 +1,6 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
-import { getAuth, signInWithPopup, OAuthProvider } from 'firebase/auth';
+import { getAuth} from 'firebase/auth';
 
 export const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -11,21 +11,5 @@ export const firebaseConfig = {
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// const provider = new OAuthProvider('microsoft.com');
-
-// signInWithPopup(auth, provider)
-//   .then((result) => {
-//     // User is signed in.
-//     // IdP data available in result.additionalUserInfo.profile.
-
-//     console.log(result)
-//     // Get the OAuth access token and ID Token
-//     const credential = OAuthProvider.credentialFromResult(result);
-//     const accessToken = credential?.accessToken;
-//     const idToken = credential?.idToken;
-//   })
-//   .catch((error) => {
-//     // Handle error.
-//   });
 const app = firebase.initializeApp(firebaseConfig);
 export const auth = getAuth(app);
